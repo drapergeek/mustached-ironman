e4a169c179de069979686243b4dab771047fbbb6
Jason Draper <jason@drapergeek.com>
2012-05-14 19:48:03 -0400

    intial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MustachedIronman::Application.config.secret_token = 'a06fa278c5964112d2bfea51f78a9d2ec3d10528dd0c589b30050adeb672f8f440292f1c35cd72684d009cc181a604234358262164b07c6d73c3bb4909dae824'
